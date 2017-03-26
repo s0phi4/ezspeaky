dccee7149d89e3cadb3591b5819748e5a04bc120
Yenifer Prajapati <yen.a@me.com>
2017-03-26 10:12:33 -0400

    Set up working environment

--- config/environment.js
+++ config/environment.js
@@ -2,11 +2,12 @@
 
 module.exports = function (environment) {
   'use strict';
-  const ENV = {
+  var ENV = {
     modulePrefix: 'ezspeaky',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    apiHost: 'http://localhost:3000/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -49,7 +50,9 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
+    ENV.rootURL = '/ezspeaky';
     ENV.locationType = 'hash';
+    ENV.apiHost = 'https://ezspeaky.herokuapp.com/';
   }
 
   return ENV;
