16f762150feb4d1128e5a232745a3e3b1e443bc4
Yenifer Prajapati <yen.a@me.com>
2017-03-29 18:18:30 -0400

    -add relationship to therapy model between therapy and videos. -Create video model and specify attributes. -Update therapy route -specify therapy_id - Add each method to therapies to display videos that correspond to the therapies themselves -Update template therapy to link to each therapy and the videos -Tests autogenerated

--- app/models/video.js
+++ app/models/video.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  videos: DS.hasMany('video'),
+  url: DS.attr('string'),
+  therapy: DS.belongsTo ('therapy'),
 });
