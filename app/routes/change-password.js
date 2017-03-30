933f90cff096d871eab180ef92086ccc09c4c526
Yenifer Prajapati <yen.a@me.com>
2017-03-30 18:05:27 -0400

    Add protection to access password
    Added model to inject authentication

--- app/routes/change-password.js
+++ app/routes/change-password.js
@@ -3,23 +3,32 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
   flashMessages: Ember.inject.service(),
+  model() {
+    if (this.get('isAuthenticated')) {
+      return this.get('store').findAll('dataset');
+    } else {
+      this.transitionTo('sign-in');
+      this.get('flashMessages')
+        .danger('Please sign in to view this page.');
+    }
+  },
 
   actions: {
-    changePassword (passwords) {
+    changePassword(passwords) {
       this.get('auth').changePassword(passwords)
-      .then(() => this.get('auth').signOut())
-      .then(() => this.transitionTo('sign-in'))
-      .then(() => {
-        this.get('flashMessages')
-        .success('Successfully changed your password!');
-      })
-      .then(() => {
-        this.get('flashMessages').warning('You have been signed out.');
-      })
-      .catch(() => {
-        this.get('flashMessages')
-        .danger('There was a problem. Please try again.');
-      });
+        .then(() => this.get('auth').signOut())
+        .then(() => this.transitionTo('sign-in'))
+        .then(() => {
+          this.get('flashMessages')
+            .success('Successfully changed your password!');
+        })
+        .then(() => {
+          this.get('flashMessages').warning('You have been signed out.');
+        })
+        .catch(() => {
+          this.get('flashMessages')
+            .danger('There was a problem. Please try again.');
+        });
     },
   },
 });
