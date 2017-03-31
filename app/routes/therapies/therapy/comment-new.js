223ab4d3ca8db99fa17ba976a87ce1a9395c95e9
Yenifer Prajapati <yen.a@me.com>
2017-03-31 14:07:54 -0400

    Fix bug

--- app/routes/therapies/therapy/comment-new.js
+++ app/routes/therapies/therapy/comment-new.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  flashMessages: Ember.inject.service(),
 
   actions: {
     createComment(data) {
       let comment = this.get('store').createRecord('comment', data);
-      comment.save();
+      comment.save()
+      .catch(() => {
+        comment.destroyRecord();
+        this.get('flashMessages')
+        .danger('There was a problem. Please enter title and comment in fields.');
+      });
     },
   },
 });
